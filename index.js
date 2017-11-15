24184a922b2456ee10b4078959198a8e840cc06e
Ashok Dey <ashokdey100@gmail.com>
2017-11-16 02:17:37 +0530

    --fixes & --updates

--- index.js
+++ index.js
@@ -4,149 +4,135 @@
  * @param senderId
  * @param route : Value can be 1 for Promotional Router or 4 for Transactional Route
  */
-module.exports = function (authKey, senderId, route) {
+module.exports = function MSG91(authKey, senderId, route) {
+  if (!authKey) {
+    throw new Error('MSG91 Authorization Key not provided.');
+  }
 
-    if (authKey == null || authKey == "") {
-        throw new Error("MSG91 Authorization Key not provided.");
+  if (!senderId) {
+    throw new Error('MSG91 Sender Id is not provided.');
+  }
+
+  if (!route) {
+    throw new Error('MSG91 router Id is not provided.');
+  }
+
+  this.send = function (mobileNos, message, callback) {
+    callback = modifyCallbackIfNull(callback);
+
+    mobileNos = validateMobileNos(mobileNos);
+
+    message = validateMessage(message);
+
+    const isUnicode = isUnicodeString(message);
+
+    // for fixing this issue - http://help.msg91.com/article/59-problem-with-plus-sign-api-send-sms
+    // EncodeUriComponent Doesn't work on ! * ( ) . _ - ' ~ `
+    const specialChars = ['+', '&', '#', '%', '@', '/', ';', '=', '?', '^', '|'];
+
+    if (specialChars.some(v => message.indexOf(v) >= 0)) {
+      // if there is at least one special character present in message string
+      message = encodeURIComponent(encodeURIComponent(message));
     }
 
-    if (senderId == null || senderId == "") {
-        throw new Error("MSG91 Sender Id is not provided.");
+    let postData = `authkey=${authKey}&sender=${senderId}&mobiles=${mobileNos }&message=${message}&route=${route}`;
+
+    if (isUnicode) {
+      postData += '&unicode=1';
     }
 
-    if (route == null || route == "") {
-        throw new Error("MSG91 router Id is not provided.");
-    }
-
-    this.send = function (mobileNos, message, callback) {
-
-        callback = modifyCallbackIfNull(callback);
-
-        mobileNos = validateMobileNos(mobileNos);
-
-        message = validateMessage(message);
-
-        var isUnicode = isUnicodeString(message);
-        
-        // for fixing this issue - http://help.msg91.com/article/59-problem-with-plus-sign-api-send-sms
-        
-        var specialChars = ['+', '&', '#', '%', '@', '/', ';', '=', '?', '^', '|']; // EncodeUriComponent Doesn't work on ! * ( ) . _ - ' ~ `
-        
-        if (specialChars.some(function (v) {
-                return message.indexOf(v) >= 0;
-            })) {
-            // if there is at least one special character present in message string
-            message = encodeURIComponent(encodeURIComponent(message));
-        }
-
-        var postData = "authkey=" + authKey + "&sender=" + senderId + "&mobiles=" + mobileNos + "&message=" + message + "&route=" + route;
-
-        if(isUnicode){
-            postData += "&unicode=1";
-        }
-
-        var options = {
-            hostname: 'control.msg91.com',
-            port: 80,
-            path: '/api/sendhttp.php',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': postData.length
-            }
-        };
-
-        makeHttpRequest(options, postData, function(err, data){
-            callback(err, data);
-        });
-
-
-
+    const options = {
+      hostname: 'control.msg91.com',
+      port: 80,
+      path: '/api/sendhttp.php',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': postData.length,
+      },
     };
 
-    this.getBalance = function(customRoute, callback) {
+    makeHttpRequest(options, postData, (err, data) => {
+      callback(err, data);
+    });
+  };
 
-        if(arguments.length == 1) {
-            callback = customRoute;
-            customRoute = null;
-        }
-
-        callback = modifyCallbackIfNull(callback);
-
-        var currentRoute = customRoute || route;
-
-        var options = {
-            hostname: 'control.msg91.com',
-            port: 80,
-            path: '/api/balance.php?authkey=' + authKey + '&type=' + currentRoute,
-            method: 'GET'
-        };
-
-        makeHttpRequest(options, null, function(err, data){
-            callback(err, data);
-        });
-
+  this.getBalance = function (customRoute, callback) {
+    if (arguments.length == 1) {
+      callback = customRoute;
+      customRoute = null;
     }
 
-    return this;
+    callback = modifyCallbackIfNull(callback);
 
+    const currentRoute = customRoute || route;
+
+    const options = {
+      hostname: 'control.msg91.com',
+      port: 80,
+      path: `/api/balance.php?authkey=${authKey}&type=${currentRoute}`,
+      method: 'GET',
+    };
+
+    makeHttpRequest(options, null, (err, data) => {
+      callback(err, data);
+    });
+  };
+
+  return this;
 };
 
-function validateMobileNos(mobileNos){
+function validateMobileNos(mobileNos) {
+  if (mobileNos == null || mobileNos == '') {
+    throw new Error('MSG91 : Mobile No is not provided.');
+  }
 
-    if (mobileNos == null || mobileNos == "") {
-        throw new Error("MSG91 : Mobile No is not provided.");
-    }
+  if (mobileNos instanceof Array) {
+    mobileNos = mobileNos.join(',');
+  }
 
-    if(mobileNos instanceof Array){
-        mobileNos = mobileNos.join(",");
-    }
-
-    return mobileNos
+  return mobileNos;
 }
 
-function validateMessage(message){
+function validateMessage(message) {
+  if (message == null || message == '') {
+    throw new Error('MSG91 : message is not provided.');
+  }
 
-    if (message == null || message == "") {
-        throw new Error("MSG91 : message is not provided.");
-    }
-
-    return message;
+  return message;
 }
 
-function modifyCallbackIfNull(callback){
-    return callback || function(){};
+function modifyCallbackIfNull(callback) {
+  return callback || function () {};
 }
 
 function isUnicodeString(str) {
-    for (var i = 0, n = str.length; i < n; i++) {
-        if (str.charCodeAt( i ) > 255) { return true; }
-    }
-    return false;
+  for (let i = 0, n = str.length; i < n; i++) {
+    if (str.charCodeAt(i) > 255) { return true; }
+  }
+  return false;
 }
 
 function makeHttpRequest(options, postData, callback) {
-
-    var http = require("http");
-    var data = "";
-    var req = http.request(options, function (res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            data += chunk;
-        });
-        res.on('end', function () {
-            callback(null, data);
-        })
+  const http = require('http');
+  let data = '';
+  const req = http.request(options, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      data += chunk;
     });
-
-    req.on('error', function (e) {
-        callback(e);
+    res.on('end', () => {
+      callback(null, data);
     });
+  });
 
-    if(postData!=null){
-        req.write(postData);
-    }
+  req.on('error', (e) => {
+    callback(e);
+  });
 
-    req.end();
+  if (postData != null) {
+    req.write(postData);
+  }
 
+  req.end();
 }
