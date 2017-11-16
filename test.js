fd1ed5771a36cbc23e6a97e0cec1a7f45844a2a1
Ashok Dey <ashokdey100@gmail.com>
2017-11-16 13:07:43 +0530

    --update : updated eamples

--- test.js
+++ test.js
@@ -1,39 +1,43 @@
 /**
  * Created by hiren on 26/11/15.
+ * Modified by Ashok Dey <github.com/ashokdey> on 16/11/2017
  */
+
 const API_KEY = null;
 const SENDER_ID = null;
 const ROUTE = null;
 
 const msg91 = require('./index')(API_KEY, SENDER_ID, ROUTE);
 
+// put your mobile number here
 const mobileNo = 'XXXXXXXXXX';
 
-msg91.send(mobileNo, 'MESSAGE', (err, response) => {
-  console.log(err);
-  console.log(response);
-});
+// sending message to a single number
+msg91.send(mobileNo, 'MESSAGE')
+  .then(response => console.log(response))
+  .catch(err => console.log(err));
 
-
+// can also send sms to an array of numbers
 const mobileList = ['XXXXXXXXXX', 'XXXXXXXXXX', 'XXXXXXXXXX'];
 
-msg91.send(mobileList, 'MESSAGE', (err, response) => {
-  console.log(err);
-  console.log(response);
-});
+msg91.send(mobileList, 'MESSAGE')
+  .then(response => console.log(response))
+  .catch(err => console.log(err));
 
+// mobile numbers can be inside a CSV file
 const mobileNoCSV = 'XXXXXXXXXX,XXXXXXXXXX,XXXXXXXXXX';
 
-msg91.send(mobileNoCSV, 'MESSAGE', (err, response) => {
-  console.log(err);
-  console.log(response);
-});
+// pass the CSV file containing mobile numbers
+msg91.send(mobileNoCSV, 'MESSAGE')
+  .then(response => console.log(response))
+  .catch(err => console.log(err));
 
-msg91.getBalance((err, response) => {
-  console.log(err);
-  console.log(response);
-});
+// check your balance
+msg91.getBalance()
+  .then(response => console.log(response))
+  .catch(err => console.log(err));
 
+// get balance for a particular route
 msg91.getBalance(ROUTE)
   .then(response => console.log(response))
   .catch(err => console.log(err));
