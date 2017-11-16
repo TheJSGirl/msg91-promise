1f5b875bf88ef42f5faf74cbc1f767d03aadd9e0
Ashok Dey <ashokdey100@gmail.com>
2017-11-16 12:46:27 +0530

    --update : updated test samples

--- test.js
+++ test.js
@@ -1,37 +1,39 @@
 /**
  * Created by hiren on 26/11/15.
  */
+const API_KEY = null;
+const SENDER_ID = null;
+const ROUTE = null;
 
-const msg91 = require('./index')('183855AEOEBOtm5a0c3b14', 'CCPLCK', 4);
+const msg91 = require('./index')(API_KEY, SENDER_ID, ROUTE);
 
-// const mobileNo = '918826624872';
+const mobileNo = 'XXXXXXXXXX';
 
-// msg91.send(mobileNo, 'MESSAGE', (err, response) => {
-//   console.log(err);
-//   console.log(response);
-// });
+msg91.send(mobileNo, 'MESSAGE', (err, response) => {
+  console.log(err);
+  console.log(response);
+});
 
 
-// const mobileList = ['XXXXXXXXXX', 'XXXXXXXXXX', 'XXXXXXXXXX'];
+const mobileList = ['XXXXXXXXXX', 'XXXXXXXXXX', 'XXXXXXXXXX'];
 
-// msg91.send(mobileList, 'MESSAGE', (err, response) => {
-//   console.log(err);
-//   console.log(response);
-// });
+msg91.send(mobileList, 'MESSAGE', (err, response) => {
+  console.log(err);
+  console.log(response);
+});
 
-// const mobileNoCSV = 'XXXXXXXXXX,XXXXXXXXXX,XXXXXXXXXX';
+const mobileNoCSV = 'XXXXXXXXXX,XXXXXXXXXX,XXXXXXXXXX';
 
-// msg91.send(mobileNoCSV, 'MESSAGE', (err, response) => {
-//   console.log(err);
-//   console.log(response);
-// });
+msg91.send(mobileNoCSV, 'MESSAGE', (err, response) => {
+  console.log(err);
+  console.log(response);
+});
 
-// msg91.getBalance((err, response) => {
-//   console.log(err);
-//   console.log(response);
-// });
+msg91.getBalance((err, response) => {
+  console.log(err);
+  console.log(response);
+});
 
-msg91.getBalance(4)
+msg91.getBalance(ROUTE)
   .then(response => console.log(response))
   .catch(err => console.log(err));
-
